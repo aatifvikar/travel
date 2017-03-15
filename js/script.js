1ba7b39ce3a1a30e5a31e13462a2dba4b2804876
Khan <aatkhan@deloitte.com>
2017-03-15 14:52:04 +0530

    adding checks for tablets and phones

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
+
+
 $(document).ready(function() {
- 
-   $(".button-click").click(function(e){
+//Checking if the user has clicked on launch-modal class and checks the attribute data and appends it in the modal title. 
+   $(".launch-modal").click(function(e){
         $('#modal-title').html($(this).attr('data'));
         $("#myModal").modal('show');
     });
